63611becd27c66ea556889417905f021a86ebd25
Benson-14 <bensonsavio14@gmail.com>
2023-09-15 13:09:26 +0530

    Images component updated

--- src/components/Pills-Components/Images.vue
+++ src/components/Pills-Components/Images.vue
@@ -8,23 +8,34 @@
       <h1 class="pl-8 py-3 bg-slate-100 text-sm">Images</h1>
 
         <div>
-        
-        <div class="w-2/5 p-4 ml-4 mt-8 h-52 bg-white rounded shadow flex flex-col">
-          <div class="flex justify-end "><button v-if="imageStore.imagePreviewUrl" @click="imageStore.removeImage" class=" text-white bg-red-500 rounded-full px-2">X</button></div>
-          
-          <img v-if="imageStore.imagePreviewUrl" :src="imageStore.imagePreviewUrl" alt="Uploaded Image" class="w-full h-full object-cover" :style="{  'border-radius': '55%',width: '100%', height: '100%'}" />
-          <input v-else type="file" id="profileImageInput" class="mb-4" @change="imageStore.handleImageUpload">
-          
+          <div class="w-auto p-4 ml-4 mt-8 h-52 bg-white rounded shadow flex flex-col md:w-3/5 lg:w-2/5 ">
+            <div class="flex justify-end">
+              <button v-if="imageStore.imagePreviewUrl" @click="imageStore.removeImage" class="text-white bg-red-500 rounded-full px-2">X</button>
+            </div>
+
+            <!-- Image Placeholder with Upload Message -->
+            <div v-if="!imageStore.imagePreviewUrl" class="flex items-center justify-center w-full h-full border-dashed border-gray-300 border-2 rounded-md">
+              <label for="profileImageInput" class="text-gray-500 text-center cursor-pointer">
+                <div>Upload Your Image</div>
+                <img src="@/assets/upload.png" alt="Upload Icon" class="w-16 h-16 mx-auto my-2" />
+                <input type="file" id="profileImageInput" class="hidden" @change="imageStore.handleImageUpload">
+              </label>
+            </div>  
+            <img v-if="imageStore.imagePreviewUrl" :src="imageStore.imagePreviewUrl" alt="Uploaded Image" class="w-full h-full object-cover" :style="{ 'border-radius': '55%', width: '100%', height: '100%' }" />
         </div>
+
+        
 
 
           <!-- Image Width Slider and Link Input Section -->
         <div class="w-3/5 p-4 ml-4 mt-4 bg-white rounded">
+          
+
           <!-- Image Width Slider -->
           <!-- Image Size Input Section -->
-          <div class="w-3/5 p-4 mt-8 bg-white rounded">
-          <h2 class="text-sm font-normal mb-2 w-24">Image Size</h2>
-          <input type="range" id="imageSizeSlider" min="10" max="300" step="1" v-model="imageStore.imageSize">
+          <div class="w-4/5 p-4 mt-8 bg-white rounded">
+            <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image Size</label>
+            <input id="imageSizeSlide" type="range" min="10" max="300" step="1" v-model="imageStore.imageSize" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
           </div>
 
           <!-- Link Input Section -->
@@ -40,18 +51,30 @@
             <a href="https://www.canva.com/" target="_blank" class="px-4 py-2 w-full bg-blue-500 text-white text-center rounded inline-block">Design on Canva</a>
           </div>
 
-          <div class="w-3/5 p-4 ml-4 my-4 h-44 bg-white rounded shadow flex flex-col">
-            <!-- Full-width Image Selector -->
-            <div class="flex justify-end"><button v-if="imageStore.bannerPreviewUrl" @click="imageStore.removeBannerImage" class=" text-white bg-red-500 rounded-full px-2">X</button></div>
-            <img v-if="imageStore.bannerPreviewUrl" :src="imageStore.bannerPreviewUrl" alt="Uploaded Banner" class="w-full h-full object-cover " />
-            <input v-else type="file" id="bannerImageInput" class="mb-4" @change="imageStore.handleBannerImageUpload">
+          <div class="w-auto p-4 ml-4 my-4 h-44 bg-white rounded shadow flex flex-col lg:w-3/5">
+              <!-- Banner Remove Button -->
+            <div class="flex justify-end">
+              <button v-if="imageStore.bannerPreviewUrl" @click="imageStore.removeBannerImage" class="text-white bg-red-500 rounded-full px-2">X</button>
+            </div>
+            <!-- Banner Image Placeholder with Upload Message -->
+            <div v-if="!imageStore.bannerPreviewUrl" class="flex items-center justify-center w-full h-full border-dashed border-gray-300 border-2 rounded-md">
+              <label for="bannerImageInput" class="text-gray-500 text-center cursor-pointer">
+                <div>Upload Your Banner</div>
+                <img src="@/assets/upload.png" alt="Upload Icon" class="w-16 h-16 mx-auto my-2" />
+                <input type="file" id="bannerImageInput" class="hidden" @change="imageStore.handleBannerImageUpload">
+              </label>
+            </div>
+
+            <!-- Display Uploaded Banner -->
+            <img v-if="imageStore.bannerPreviewUrl" :src="imageStore.bannerPreviewUrl" alt="Uploaded Banner" class="w-full h-full object-cover" />
           </div>
 
           <!-- Banner Width Slider and Link Input Section -->
           <div class="w-3/5 p-4 ml-4 mt-8 bg-white rounded">
             <!-- Banner Width Slider -->
-            <h2 class="text-sm font-normal mb-2 w-24">Banner Width</h2>
-            <input type="range" id="bannerWidthSlider" min="10" max="100" step="1" v-model="imageStore.bannerWidth">
+            <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Banner Size</label>
+            <input id="bannerWidthSlider" type="range" min="10" max="100" step="1" v-model="imageStore.bannerWidth" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+
 
             <!-- Link Input Section -->
             
@@ -76,6 +99,7 @@ export default {
 
   },
 };
+
 </script>
 
 
