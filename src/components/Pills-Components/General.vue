3819bf28689670c43e6d59e4fc5ffa608a135b1a
Benson-14 <bensonsavio14@gmail.com>
2023-09-15 11:44:46 +0530

    General component updated

--- src/components/Pills-Components/General.vue
+++ src/components/Pills-Components/General.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen md:flex md:flex-row-reverse md:gap-10 bg-white md:mx-10 md:pt-2 md:pb-6 md:px-12 md:justify-center md:rounded-br-lg md:rounded-bl-lg mb-6 lg:mx-36">
+  <div class=" md:flex md:flex-row-reverse md:gap-10 bg-white md:mx-10 md:pt-2 md:pb-6 md:px-12 md:justify-center md:rounded-br-lg md:rounded-bl-lg mb-6 lg:mx-36">
     <!-- Preview -->
     <Preview/>
 
@@ -8,57 +8,57 @@
     <!-- Feature -->
     <div class="mx-3 border-blue-300 border rounded-lg md:flex-1">
       <h1 class="text-base mb-6 bg-slate-100 pl-4 py-2 rounded-tr-lg rounded-tl-lg">General</h1>
+
       <!-- Name -->
-      <div class="relative mb-4 lg:mx-4 mx-2">
-        <label for="name" class="absolute -top-2 left-2 inline-block px-1 text-xs bg-white font-light text-gray-900">Name</label>
+      <div class="rounded-md px-3 pb-1 pt-1 mb-4 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-blue-600 lg:mx-4 mx-2">
+        <label for="name" class="block text-xs font-medium text-gray-900">Name</label>
         <input
           type="text"
-          name="name"
-          id="name"
+          name="name" 
+          id="name" 
           v-model="generalStore.inputName"
-          class="block w-full rounded-md border py-1.5 text-gray-900 px-2 shadow-sm outline-none sm:text-sm sm:leading-6"
-          placeholder=""
-        />
+          class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"
+          placeholder="">
       </div>
+
       <!-- Company -->
-      <div class="relative mb-4 lg:mx-4 mx-2">
-        <label for="company" class="absolute -top-2 left-2 inline-block px-1 text-xs bg-white font-light text-gray-900">Company</label>
+      <div class="rounded-md px-3 pb-1 pt-1 mb-4 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-blue-600 lg:mx-4 mx-2">
+        <label for="name" class="block text-xs font-medium text-gray-900">Company</label>
         <input
           type="text"
-          name="company"
-          id="company"
+          name="name" 
+          id="name" 
           v-model="generalStore.inputCompany"
-          class="block w-full rounded-md border py-1.5 text-gray-900 px-2 shadow-sm outline-none sm:text-sm sm:leading-6"
-          placeholder=""
-        />
+          class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"
+          placeholder="">
       </div>
+
       <!-- Position -->
-      <div class="relative mb-4 lg:mx-4 mx-2">
-        <label for="position" class="absolute -top-2 left-2 inline-block px-1 text-xs bg-white font-light text-gray-900">Position</label>
+      <div class="rounded-md px-3 pb-1 pt-1 mb-4 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-blue-600 lg:mx-4 mx-2">
+        <label for="name" class="block text-xs font-medium text-gray-900">Position</label>
         <input
           type="text"
-          name="position"
-          id="position"
+          name="name" 
+          id="name" 
           v-model="generalStore.inputPosition"
-          class="block w-full rounded-md border py-1.5 text-gray-900 px-2 shadow-sm outline-none sm:text-sm sm:leading-6"
-          placeholder=""
-        />
+          class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"
+          placeholder="">
       </div>
+
       <!-- Department -->
-      <div class="relative mb-4 lg:mx-4 mx-2">
-        <label for="department" class="absolute -top-2 left-2 inline-block px-1 text-xs bg-white font-light text-gray-900">Department</label>
+      <div class="rounded-md px-3 pb-1 pt-1 mb-4 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-blue-600 lg:mx-4 mx-2">
+        <label for="name" class="block text-xs font-medium text-gray-900">Department</label>
         <input
           type="text"
-          name="department"
-          id="department"
+          name="name" 
+          id="name" 
           v-model="generalStore.inputDepartment"
-          class="block w-full rounded-md border py-1.5 text-gray-900 px-2 shadow-sm outline-none sm:text-sm sm:leading-6"
-          placeholder=""
-        />
+          class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"
+          placeholder="">
       </div>
 
       <h1 class="text-base mb-4 bg-slate-100 pl-4 py-2">Contacts</h1>
-      <div class="px-3">
+      <div class="px-3" >
         <div class="inline-block relative">
           <button class="py-1 px-2 bg-blue-500 text-white rounded-md" @click="generalStore.toggleAddFieldDropdown">
             Add Field
@@ -78,12 +78,12 @@
         </div>
 
         <div id="inputFieldsContainer" class="mt-6">
-          <div v-for="(field, index) in generalStore.inputFields" :key="index" class="mt-2 flex justify-between items-center" :style="{ paddingBottom: '10px' }">
-            <div class="flex-1 relative">
-              <label :for="field.type" class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-light text-gray-900">{{ field.type }}</label>
-              <input :type="field.type" :name="field.type" :id="field.type" v-model="field.value" class="block w-full rounded-md border px-2 py-2 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 " @focus="removeOutline"  />
-              <button class="absolute top-2 right-2 px-2 rounded-md text-lg bg-red-100 text-red-600" @click="generalStore.deleteField(index)">X</button>
+          <div v-for="(field, index) in generalStore.inputFields" :key="index" class="rounded-md px-3 pb-1 pt-1 mb-2  shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-blue-600 lg:mx-1 mt-4 flex items-center">
+            <div class="flex-1">
+              <label :for="field.type" class="block text-xs font-medium text-gray-900">{{ field.type }}</label>
+              <input :type="field.type" :name="field.type" :id="field.type" v-model="field.value" class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6" :placeholder="field.type" />
             </div>
+            <button class="ml-2 px-2 rounded-full text-lg bg-red-100 text-red-600" @click="generalStore.deleteField(index)">X</button>
           </div>
         </div>
       </div>
@@ -99,6 +99,7 @@ import Preview from '@/components/Preview.vue';
 
 export default {
   components: { Preview },
+
   data() {
     return {
       generalStore: useGeneralStore(),
