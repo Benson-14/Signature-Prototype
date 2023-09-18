b34e89b5b556bb8574ec06a4321249a06773a36c
Benson-14 <bensonsavio14@gmail.com>
2023-09-18 07:29:37 +0530

    pill updated

--- src/views/AddSignature.vue
+++ src/views/AddSignature.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="max-w-full py-8 rounded-tr-lg rounded-tl-lg hidden bg-white md:flex md:justify-center md:mx-10 lg:mx-36">
     <div class="isolate bg-slate-100 rounded-full overflow-hidden shadow">
-    <button @click="showComponent('General')" :class="{ 'selected': currentComponent === 'General' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium hover:bg-gray-50 focus:z-10"> <i class="fa-solid fa-bars mr-1"></i> General</button>
-    <button @click="showComponent('Images')" :class="{ 'selected': currentComponent === 'Images' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium hover:bg-gray-50 focus:z-10"> <i class="fa-regular fa-image mr-1 "></i>Images</button>
-    <button @click="showComponent('Socials')" :class="{ 'selected': currentComponent === 'Socials' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium hover:bg-gray-50 focus:z-10"> <i class="fa-solid fa-share-nodes mr-1"></i>Socials</button>
-    <button @click="showComponent('AddOns')" :class="{ 'selected': currentComponent === 'AddOns' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium hover:bg-gray-50 focus:z-10"> <i class="fa-solid fa-folder-plus mr-1"></i>Add-ons </button>
-    <button @click="showComponent('Design')" :class="{ 'selected': currentComponent === 'Design' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium hover:bg-gray-50 focus:z-10"><i class="fa-solid fa-brush mr-1"></i>Design</button>
+    <button @click="showComponent('General')" :class="{ 'selected': currentComponent === 'General','hover:bg-gray-50': currentComponent !== 'General' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium focus:z-10"> <i class="fa-solid fa-bars mr-1"></i> General</button>
+    <button @click="showComponent('Images')" :class="{ 'selected': currentComponent === 'Images','hover:bg-gray-50': currentComponent !== 'Images' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium focus:z-10"> <i class="fa-regular fa-image mr-1 "></i>Images</button>
+    <button @click="showComponent('Socials')" :class="{ 'selected': currentComponent === 'Socials','hover:bg-gray-50': currentComponent !== 'Socials' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium focus:z-10"> <i class="fa-solid fa-share-nodes mr-1"></i>Socials</button>
+    <button @click="showComponent('AddOns')" :class="{ 'selected': currentComponent === 'AddOns','hover:bg-gray-50': currentComponent !== 'AddOns'}" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium focus:z-10"> <i class="fa-solid fa-folder-plus mr-1"></i>Add-ons </button>
+    <button @click="showComponent('Design')" :class="{ 'selected': currentComponent === 'Design','hover:bg-gray-50': currentComponent !== 'Design' }" class="rounded-3xl py-2 px-4 mx-1 text-center text-xs font-medium focus:z-10"><i class="fa-solid fa-brush mr-1"></i>Design</button>
   </div>
   </div>
 
   <div class="fixed bottom-0 w-full bg-white shadow md:hidden">
     <div class="flex divide-x divide-gray-200 md:gap-4 md:justify-center md:divide-x-0 md:items-center">
-    <button @click="showComponent('General')" :class="{ 'selected': currentComponent === 'General' }" class="flex-1 p-2 md:rounded-l-full md:py-4 md:px-5 min-w-max text-center text-xs font-medium hover:bg-gray-200 focus:z-10"><i class="fa-solid fa-bars mr-1"></i>General</button>
-    <button @click="showComponent('Images')" :class="{ 'selected': currentComponent === 'Images' }"  class="flex-1 p-2 md:py-4 md:px-5 min-w-max text-center text-xs font-medium hover:bg-gray-200 focus:z-10"><i class="fa-regular fa-image mr-1 "></i>Images</button>
-    <button @click="showComponent('Socials')" :class="{ 'selected': currentComponent === 'Socials' }" class="flex-1 p-2 md:py-4 md:px-5 min-w-max text-center text-xs font-medium hover:bg-gray-200 focus:z-10"><i class="fa-solid fa-share-nodes mr-1"></i>Socials</button>
-    <button @click="showComponent('AddOns')" :class="{ 'selected': currentComponent === 'AddOns' }" class="flex-1 p-2 md:py-4 md:px-5 min-w-max text-center text-xs font-medium hover:bg-gray-200 focus:z-10"><i class="fa-solid fa-folder-plus mr-1"></i>Add-ons</button>
-    <button @click="showComponent('Design')" :class="{ 'selected': currentComponent === 'Design' }"  class="flex-1 p-2 md:rounded-l-full md:py-4 md:px-5 min-w-max text-center text-xs font-medium hover:bg-gray-200 focus:z-10"><i class="fa-solid fa-brush mr-1"></i>Design</button>
+    <button @click="showComponent('General')" :class="{ 'selected': currentComponent === 'General','hover:bg-gray-200': currentComponent !== 'General' }" class="flex-1 p-2 md:rounded-l-full md:py-4 md:px-5 min-w-max text-center text-xs font-medium focus:z-10"><i class="fa-solid fa-bars mr-1"></i>General</button>
+    <button @click="showComponent('Images')" :class="{ 'selected': currentComponent === 'Images','hover:bg-gray-200': currentComponent !== 'Images' }"  class="flex-1 p-2 md:py-4 md:px-5 min-w-max text-center text-xs font-medium  focus:z-10"><i class="fa-regular fa-image mr-1 "></i>Images</button>
+    <button @click="showComponent('Socials')" :class="{ 'selected': currentComponent === 'Socials','hover:bg-gray-200': currentComponent !== 'Socials'  }" class="flex-1 p-2 md:py-4 md:px-5 min-w-max text-center text-xs font-medium focus:z-10"><i class="fa-solid fa-share-nodes mr-1"></i>Socials</button>
+    <button @click="showComponent('AddOns')" :class="{ 'selected': currentComponent === 'AddOns','hover:bg-gray-200': currentComponent !== 'AddOns' }" class="flex-1 p-2 md:py-4 md:px-5 min-w-max text-center text-xs font-medium focus:z-10"><i class="fa-solid fa-folder-plus mr-1"></i>Add-ons</button>
+    <button @click="showComponent('Design')" :class="{ 'selected': currentComponent === 'Design','hover:bg-gray-200': currentComponent !== 'Design' }"  class="flex-1 p-2 md:rounded-l-full md:py-4 md:px-5 min-w-max text-center text-xs font-medium focus:z-10"><i class="fa-solid fa-brush mr-1"></i>Design</button>
   </div>
   </div>
 
